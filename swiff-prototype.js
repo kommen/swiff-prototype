a472162e39a7be4cb0ce37c3f3284194d710fb9b
Dieter Komendera <dieter@komendera.com>
2008-05-26 18:43:37 +0200

    Use Prototype's browser detection.

--- swiff-prototype.js
+++ swiff-prototype.js
@@ -56,7 +56,7 @@ var Swiff = new Class({
 		}
 
 		params.flashVars = Hash.toQueryString(vars);
-		if (Browser.Engine.trident){
+		if (Protoype.Browser.IE){
 			properties.classid = 'clsid:D27CDB6E-AE6D-11cf-96B8-444553540000';
 			params.movie = path;
 		} else {
